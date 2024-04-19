258bf15a695810a12819f2fb01af5357329a29dc
EnzoHenrico <enzo.mascia1@gmail.com>
2024-04-19 15:43:56 -0300

    Add project files.

---
+++ NumerosPrimos/Program.cs
@@ -0,0 +1,27 @@
+﻿int number = 0, count = 0;
+
+Console.Write("Diga um número inteiro para verificar se é primo: ");
+number = int.Parse(Console.ReadLine());
+
+
+for(int divider = number; divider > 0; divider--)
+{
+    if (number % divider == 0)
+    {
+        count++;
+    }
+    if (number < 1 || count > 2)
+    {
+        break;
+    }  
+}
+
+if (count == 2)
+{
+    Console.WriteLine("\nO número é primo!");
+}
+else
+{
+    Console.WriteLine("\nO número não é primo.");
+}
+
